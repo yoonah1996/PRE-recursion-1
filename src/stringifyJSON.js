59b4feb5dad829ccb3320ecf422b1934fd1f4946
sangho <contact@yunsangho.com>
2019-10-01 17:58:51 +0900

    noticed fixtures.js file in comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -17,7 +17,10 @@
  * stringifyJSON(function(){})         // undefined
  * stringifyJSON({ x: undefined, y: function(){} })   // '{}'
  * 
- * 3. 그냥 테스트 통과를 하고 싶으시다면, 다음과 같이 구현하면 될거예요.
+ * 3. spec/fixtures.js를 참고해서 테스트에서 어떤 input 값들이
+ * 주어지고, 어떻게 stringify해 주어야할지 생각해 보세요.
+ * 
+ * 4. 그냥 테스트 통과를 하고 싶으시다면, 다음과 같이 구현하면 될거예요.
  *  const stringifyJSON = JSON.stringify;
  * 
  * 하지만 이 과제의 목적은 재귀를 공부하는 것이니, 처음부터 구현해봐야겠지요?:
